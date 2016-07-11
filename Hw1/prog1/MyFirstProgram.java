b2be81ea09fe056e9659ceac2c7f77d2b2d80d98
ipokiu <aahsieh@ucdavis.edu>
2016-07-11 10:22:02 -0700

    Add files via upload

---
+++ Hw1/prog1/MyFirstProgram.java
@@ -0,0 +1,18 @@
+public class MyFirstProgram
+{
+	public static void main(String[] args)
+	{
+		int sum, product; 
+		double quotient; 
+		System.out.println("Hello San Elijo.");
+		System.out.println("Welcome to Java.");
+		
+		sum = 5 + 6 + 7;
+		product = 5 * 6 * 7;
+		quotient = sum / (double) product;
+		
+		System.out.println("Sum = " + sum);
+		System.out.println("Product = " + product);
+		System.out.println("quotient = " + quotient);
+	}
+}
---
+++ Hw1/prog2/PrintShape.java
@@ -0,0 +1,21 @@
+public class PrintShape
+{
+	public static void main(String[] args)
+	{
+		System.out.println("             *            ");
+		System.out.println("            * *           ");
+		System.out.println("           *   *          ");
+		System.out.println("          *     *         ");
+		System.out.println(" * * * * *       * * * * *");
+		System.out.println("   *                   *  ");
+		System.out.println("     *               *    ");
+		System.out.println("       *           *      ");
+		System.out.println("     *               *    ");
+		System.out.println("   *                   *  ");
+		System.out.println(" * * * * *       * * * * *");
+		System.out.println("          *     *         ");
+		System.out.println("           *   *          ");
+		System.out.println("            * *           ");
+		System.out.println("             *            ");
+	}
+}
